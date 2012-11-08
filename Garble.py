3ff02f1769c596eaaeef2d306a0f854bb4b42790
Feni Varughese <feni727@gmail.com>
2012-11-07 22:57:52 -0600

    Added a stylesheet so that the notes can be viewed easily as a site

--- Garble.py
+++ Garble.py
@@ -9,52 +9,70 @@
 # Adds support for multi-line inputs
 # Change in API to support this, by capturing the input at the method... 
 
+# Version 0.3: June 26, 2010
+# Support for formatted display in a browser
+
 
 # TODO: Switch to sqlite database for data storage
 # 	Check and smart fix the format of the notes if previous one closed before fully written... 
 # 	Support searching and sorting
 #	Add viewing tools also
 #	Auto-tagging and more system-context sensitive meta data
+#	Insert Note id and note count ect. ect. meta data. 
+
 
 import datetime	# import the datetime package to allow us to add in the time meta-data
+import os;	# Allows us to get the filesize, which allows us to seek to the position right before </root>
 
 def addNote():
 	time = datetime.datetime.now()
 
-	notes = open('GarbleNotes.txt', 'a')
-	notes.write("\n<note>\n");
+	filename = "GarbleNotes.xml";
+	fileSize = os.path.getsize(filename)
 	
-	notes.write("\t<year>");
+	if fileSize < 14:	# File formatting is somehow messed up. Doesn't contain the root nodes. 
+		notes = open(filename, 'w+');
+		notes.write("<root>\n</root>");
+		notes.close();
+
+	notes = open(filename, "r+");	
+	notes.seek(os.path.getsize(filename) - 7)	# seek to right before the </root>
+
+	# write this note in... 		
+	notes.write("\n\t<note>\n");
+	
+	notes.write("\t\t<year>");
 	notes.write(str(time.year));
 	notes.write("</year>\n");
 	
-	notes.write("\t<month>");
+	notes.write("\t\t<month>");
 	notes.write(str(time.month));
 	notes.write("</month>\n");
 	
-	notes.write("\t<day>");
+	notes.write("\t\t<day>");
 	notes.write(str(time.day));
 	notes.write("</day>\n");
 
-	notes.write("\t<hour>");
+	notes.write("\t\t<hour>");
 	notes.write(str(time.hour));
 	notes.write("</hour>\n");
 	
-	notes.write("\t<minute>");
+	notes.write("\t\t<minute>");
 	notes.write(str(time.minute));
 	notes.write("</minute>\n");
 	
-	notes.write("\t<message>");
+	notes.write("\t\t<message>");
 	
 	content = raw_input(":")
 	while content != "":
-		notes.write("\n\t\t");		# Proper spacing and tabbing... for style reasons... 
+		notes.write("\n\t\t\t");		# Proper spacing and tabbing... for style reasons... 
 		notes.write(content);
 		content = raw_input(":")
 	
-	notes.write("\n\t</message>\n");	
-
-	notes.write("</note>\n\n");
+	notes.write("\n\t\t</message>\n");	
+	notes.write("\t</note>\n");
+	
+	notes.write("</root>");	
 	notes.close();
 
 addNote();
