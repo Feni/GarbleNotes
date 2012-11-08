1d3d942396278632ddfc3aefbd8e37b8e8fefda5
Feni Varughese <feni727@gmail.com>
2012-11-07 22:56:57 -0600

    June 25 2010

--- Garble.py
+++ Garble.py
@@ -2,16 +2,23 @@
 # A Note taking program that simply appends your notes to a notes.txt file with simple meta data such as date and time... 
 # Author: Feni Varughese
 
-# Version 0.1: Outputs to a simple text file
+# Version 0.1: June 24, 2010
+# Outputs to a simple text file
+
+# Version 0.2: June 25, 2010
+# Adds support for multi-line inputs
+# Change in API to support this, by capturing the input at the method... 
+
+
 # TODO: Switch to sqlite database for data storage
+# 	Check and smart fix the format of the notes if previous one closed before fully written... 
 # 	Support searching and sorting
 #	Add viewing tools also
 #	Auto-tagging and more system-context sensitive meta data
 
 import datetime	# import the datetime package to allow us to add in the time meta-data
 
-
-def addNote(content):
+def addNote():
 	time = datetime.datetime.now()
 
 	notes = open('GarbleNotes.txt', 'a')
@@ -37,12 +44,17 @@ def addNote(content):
 	notes.write(str(time.minute));
 	notes.write("</minute>\n");
 	
-	notes.write("\t<message>\n\t\t");
-	notes.write(content);
+	notes.write("\t<message>");
+	
+	content = raw_input(":")
+	while content != "":
+		notes.write("\n\t\t");		# Proper spacing and tabbing... for style reasons... 
+		notes.write(content);
+		content = raw_input(":")
+	
 	notes.write("\n\t</message>\n");	
 
 	notes.write("</note>\n\n");
 	notes.close();
 
-newNote = raw_input(":");
-addNote(newNote);
+addNote();
